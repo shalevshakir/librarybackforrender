98e0e3364eca4a45637fd6f378f5e15f3a0f674a
shalevshakir <shalevshakir@gmail.com>
2025-02-02 14:03:46 +0200

    new procofile

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, timedelta
+import os
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///library.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG']= False
 db = SQLAlchemy(app)
